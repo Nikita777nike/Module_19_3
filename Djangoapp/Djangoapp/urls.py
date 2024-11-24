26874529bbe551e96a455954a136f925946d9caf
Nikita777nike <sem.nike@yandex.ru>
2024-11-24 10:37:24 +0300

    add fail

--- Djangoapp/Djangoapp/urls.py
+++ Djangoapp/Djangoapp/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task1.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page),
+    path('game/', game_page),
+    path('cart/', cart_page),
+    path('sing/', sign_up_by_django),
 ]
